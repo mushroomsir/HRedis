e9fabb030103a9851b071b2b9986a182ada9fc87
mushroomsir <mushroomsir@163.com>
2015-04-20 18:38:32 +0800

    RedisSentinel

---
+++ HRedis/RedisSentinel.cs
@@ -0,0 +1,22 @@
+﻿
+namespace HRedis
+{
+    public class RedisSentinel : RedisBaseClient
+    {
+         public RedisSentinel(RedisConfiguration config)
+            : base(config)
+        {
+           
+        }
+
+         public RedisSentinel(string ip, int port)
+            : this(new RedisConfiguration()
+            {
+                Host = ip,
+                Port = port,
+            })
+        {
+
+        }
+    }
+}
